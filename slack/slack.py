3b0a81c1ca0181064c230c6644da7919a95bff21
Shanmukh Kotharu <43720664+shanmukhkotharu@users.noreply.github.com>
2021-03-20 22:45:09 +0530

    Add files via upload

---
+++ slack/slack.py
@@ -0,0 +1,29 @@
+import os
+import logging
+import urllib
+from botocore.exceptions import ClientError
+
+def handler(channel_id,message):
+    # bot token
+    BOT_TOKEN = os.environ["BOT_TOKEN"]
+    # place url from slack here.
+    SLACK_URL = "Replace_this"
+    channel_id = slack_event["channel"]
+    data = urllib.parse.urlencode(
+                (
+                    ("token", BOT_TOKEN),
+                    ("channel", channel_id),
+                    ("text", message)
+                )
+            )
+    data = data.encode("ascii")
+    request = urllib.request.Request(
+            SLACK_URL,
+            data=data,
+            method="POST"
+        )
+    request.add_header(
+            "Content-Type",
+            "application/x-www-form-urlencoded"
+        )
+    urllib.request.urlopen(request).read()
